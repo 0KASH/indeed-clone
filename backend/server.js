10fd9de697bd5e0bf5b99c62c30a39323de7f243
Muhammad Shahid <shahid.mern.stack@gmail>
2026-07-15 06:19:46 -0700

    Implement user authentication with login and password hashing

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import  dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config()
 
@@ -13,6 +14,9 @@ connectDB()
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+
+app.use("/api/auth", authRoutes);
 
 
 //Test API
